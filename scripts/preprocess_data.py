350053a8ac9360fc5b291ef9000565f2b32beb82
Jakub-Polczyk-PWr <276942@student.pwr.edu.pl>
2023-04-22 12:46:35 +0200

    Preprocessing zbioru geolokalizacji
    * odpalenie skryptów związanych z wizualizacjami grafów na mapach będzie wymagało użycia środowiska Conda

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import numpy as np
 import pandas as pd
 from datetime import datetime
 
@@ -14,11 +15,33 @@ COLUMNS = [
     "duration",
 ]
 
+GEOLOCATION_COLUMNS = [
+    "station_number",
+    "station_name",
+    "number_of_bikes_available",
+    "number_of_bikes_racks",
+    "latitude",
+    "longitude",
+]
+
+GEOLOCATION_COLUMNS_TO_DROP = ["Współrzędne", "Numery dostępnych rowerów"]
+
 
 def process_dataset(data: pd.DataFrame) -> pd.DataFrame:
     data.columns = COLUMNS
     indices = data[data.isin(["Poza stacją"]).any(axis=1)].index
     return data.drop(indices)
+
+
+def process_geolocation_dataset(df_geolocation: pd.DataFrame) -> pd.DataFrame:
+    latitude, longitude = np.vectorize(get_latitude_longitude)(
+        df_geolocation["Współrzędne"]
+    )
+    df_geolocation["latitude"] = latitude
+    df_geolocation["longitude"] = longitude
+    df_geolocation.drop(GEOLOCATION_COLUMNS_TO_DROP, axis=1, inplace=True)
+    df_geolocation.columns = GEOLOCATION_COLUMNS
+    return df_geolocation
 
 
 def get_ride_durations_in_mins(
@@ -33,7 +56,13 @@ def get_ride_durations_in_mins(
     return durations
 
 
+def get_latitude_longitude(geolocation: str):
+    latitude, longitude = geolocation.split(", ")
+    return float(latitude), float(longitude)
+
+
 if __name__ == "__main__":
+    # processing dataset
     datasets = []
     path = os.getcwd()
     files = glob.glob(os.path.join(path, "dataset/raw/historia_przejazdow_*"))
@@ -49,4 +78,12 @@ if __name__ == "__main__":
 
     pd.concat(datasets).reset_index(drop=True).to_csv(
         "dataset/processed/processed_dataset.csv", index=False
+    )
+
+    # processing geolocation dataset
+    geolocation_file_path = "dataset/raw/stacje_rowerowe.csv"
+    df_geolocation = pd.read_csv(geolocation_file_path)
+    df_geolocation_processed = process_geolocation_dataset(df_geolocation)
+    df_geolocation_processed.to_csv(
+        "dataset/processed/processed_geolocation_dataset.csv", index=False
     )
